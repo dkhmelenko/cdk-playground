14fcd29dde6add9393d1bd896a63a8a20ebb33e7
Dmytro Khmelenko <d.khmelenko@gmail.com>
2021-10-13 10:10:15 +0200

    Initial commit

--- cdk_example/cdk_example_stack.py
+++ cdk_example/cdk_example_stack.py
@@ -1,15 +1,22 @@
-from aws_cdk import core as cdk
+from aws_cdk import (
+    aws_s3,
+    aws_s3_notifications,
+    core,
+    aws_sns
+)
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
 
+class CdkExampleStack(core.Stack):
 
-class CdkExampleStack(cdk.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+        # create S3 bucket
+        s3 = aws_s3.Bucket(self, "s3bucket_cdk")
 
-        # The code that defines your stack goes here
+        # define SNS topic ans assign it
+        sns_topic = aws_sns.Topic(self, "CDK Notification")
+        sns_notification = aws_s3_notifications.SnsDestination(sns_topic)
+
+        # assign notification for the s3 event type when new object created
+        s3.add_event_notification(aws_s3.EventType.OBJECT_CREATED, sns_notification)
